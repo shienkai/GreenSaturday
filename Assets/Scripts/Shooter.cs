f6a7e2d0f6100458241f7f047f42a0f43e3800e2
Syuu0118 <a1024mrkrs@icloud.com>
2023-11-07 15:09:54 +0900

    117コードやりましたね。 ・射出機能追加

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+ public GameObject candyPrefab;
+ public float shotForce;
+ public float shotTorque;
+
+ void Update()
+ {
+    if (Input.GetButtonDown("Fire1"))Shot();
+ }
+
+ public void Shot()
+ {
+    GameObject candy = Instantiate(
+        candyPrefab,
+        transform.position,
+        Quaternion.identity
+    );
+    Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
+    candyRigidBody.AddForce(transform.forward * shotForce);
+    candyRigidBody.AddTorque(new Vector3(0,shotTorque,0));
+ }
+}
