1778f02f3bdb37b7128eb0071cc5caac8f589ce5
Syuu0118 <a1024mrkrs@icloud.com>
2023-11-21 14:59:44 +0900

    ・落としたら消える機能の追加 ・単位の追加 ・個数の表示を追加

---
+++ Assets/Scripts/CandyDestroye.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+   {
+        if (other.gameObject.tag == "Candy")
+     {
+        Destroy(other.gameObject);
+     }
+ }
+}
---
+++ Assets/Scripts/CandyManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyManager : MonoBehaviour
+{
+    const int DefaultCandyAmount = 30;
+
+    public int candy = DefaultCandyAmount;
+
+    public void ConsumeCandy()
+    {
+        if (candy > 0) candy--;
+    }
+
+    public int GetCandyAmount()
+    {
+        return candy;
+    }
+
+    public void AddCandy(int amount)
+    {
+        candy += amount;
+    }
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        
+        string label = "Candy :" + candy;
+
+        GUI.Label(new Rect(50,50,100,30),label);
+    }
+}
